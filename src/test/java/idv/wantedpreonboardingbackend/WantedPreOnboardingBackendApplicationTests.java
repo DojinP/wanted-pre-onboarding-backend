a2e5f24fdd2ad245be7e00e8a016110cc141fcdf
Dojin Park <coah2013@naver.com>
2023-10-12 18:34:06 +0900

    [test] 채용공고 등록 API 테스트
    채용공고 등록 API 테스트

--- src/test/java/idv/wantedpreonboardingbackend/WantedPreOnboardingBackendApplicationTests.java
+++ src/test/java/idv/wantedpreonboardingbackend/WantedPreOnboardingBackendApplicationTests.java
@@ -1,13 +1,27 @@
 package idv.wantedpreonboardingbackend;
 
+import idv.wantedpreonboardingbackend.jobopening.domain.JobopeningEntity;
+import idv.wantedpreonboardingbackend.jobopening.repository.JobopeningRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class WantedPreOnboardingBackendApplicationTests {
 
+	@Autowired
+	JobopeningRepository jobopeningRepository;
+
 	@Test
-	void contextLoads() {
+	void save() {
+
+		JobopeningEntity jobopeningEntity = new JobopeningEntity(1L,
+												"백엔드 개발자",
+												100000000L,
+													"10년 경력 환영",
+													"Java, Spring");
+
+		jobopeningRepository.save(jobopeningEntity);
 	}
 
 }
