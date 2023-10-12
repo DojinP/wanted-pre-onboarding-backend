6eed7a0cb680182d261c72c2c73e3ef15dd09480
Dojin Park <coah2013@naver.com>
2023-10-12 18:33:39 +0900

    [refactor] Entity 컬럼명 변경 및 생성자 정의
    Spring Table name 생성규칙 변경에 따른 Entity 컬럼명 변경
    생성자 정의

--- src/main/java/idv/wantedpreonboardingbackend/jobopening/domain/JobopeningEntity.java
+++ src/main/java/idv/wantedpreonboardingbackend/jobopening/domain/JobopeningEntity.java
@@ -3,9 +3,12 @@ package idv.wantedpreonboardingbackend.jobopening.domain;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@RequiredArgsConstructor
 @Entity
 @Table(name = "JOBOPENING")
 public class JobopeningEntity {
@@ -13,10 +16,15 @@ public class JobopeningEntity {
     @Column(name = "ID")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String companyId;   // 회사 id
-    private String recruitmentPosition; // 채용 포지션
-    private Long recruitmentCompensation;    // 채용 보상금
-    private String recruitmentInfo; // 채용내용
-    private String techInUse;   // 사용 기술
+    @NonNull
+    private Long company_id;   // 회사 id
+    @NonNull
+    private String recruitment_position; // 채용 포지션
+    @NonNull
+    private Long recruitment_compensation;    // 채용 보상금
+    @NonNull
+    private String recruitment_info; // 채용 내용
+    @NonNull
+    private String tech_in_use;   // 사용 기술
 
 }
