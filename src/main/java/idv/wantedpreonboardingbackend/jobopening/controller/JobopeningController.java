b9c252a78fed12ecb003f3ffeb0daacc9e03f880
Dojin Park <coah2013@naver.com>
2023-10-13 19:10:40 +0900

    [refactor/feat] Update API 작성 및 리팩토링
    Update API 작성
    Service 단 Transaction 처리
    공고등록 엔티티 구조 변경
     - 기본 생성자 추가
     - 기본키 제외한 값 생성자 추가

--- src/main/java/idv/wantedpreonboardingbackend/jobopening/controller/JobopeningController.java
+++ src/main/java/idv/wantedpreonboardingbackend/jobopening/controller/JobopeningController.java
@@ -2,23 +2,36 @@ package idv.wantedpreonboardingbackend.jobopening.controller;
 
 import idv.wantedpreonboardingbackend.jobopening.domain.JobopeningEntity;
 import idv.wantedpreonboardingbackend.custom.Response;
+import idv.wantedpreonboardingbackend.jobopening.domain.UpdateJobopeningDTO;
 import idv.wantedpreonboardingbackend.jobopening.service.JobopeningService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/jobopening")
 public class JobopeningController {
     @Autowired
     JobopeningService jobopeningService;
 
     // 채용공고 등록
+    @PostMapping()
     public Response<?> registerJobopening(@RequestBody JobopeningEntity jobopeningEntity){
+        System.out.println("요청 확인!");
+        System.out.println(jobopeningEntity);
         return new Response<>("true", "등록 성공", jobopeningService.registerJobopening(jobopeningEntity));
     }
     // 채용공고 수정
+    @PatchMapping("/{id}")
+    public Response<?> updateJobopening(@RequestBody UpdateJobopeningDTO updateJobopeningDTO, @PathVariable Long id){
+        return new Response<>("true", "수정 성공", jobopeningService.updateJobopening(updateJobopeningDTO, id));
+    }
 
     // 채용공고 삭제
+    @DeleteMapping("/{id}")
+    public Response<?> deleteJobopening(@PathVariable Long id){
+        return new Response<>("true", jobopeningService.deleteJobopening(id), null);
+    }
 
     // 채용공고 조회
 
